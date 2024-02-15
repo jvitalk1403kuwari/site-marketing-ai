fd6d92da7d0a3c14d0024d503b2a4d276501f64d
Kouwari <111056495+jvitalk1403kuwari@users.noreply.github.com>
2024-02-15 23:03:36 +0100

    Scroll Top Button

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,26 @@ const toggleMenu = () => menuElementsToToggle.forEach(el => el.classList.toggle(
 
 iconeToggle.addEventListener("click" , toggleMenu);
 
+
+
+//-------------Fonction pour remonter en haut de la page-------------//
+// Fonction pour remonter en haut de la page
+function scrollTopFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  
+  // Afficher ou masquer le bouton en fonction de la position de défilement
+  window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    var btn = document.getElementById("scrollTopBtn");
+  
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  }
