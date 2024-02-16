157a149d76d3dd64a389259237b08312d8ba5d9a
Kouwari <111056495+jvitalk1403kuwari@users.noreply.github.com>
2024-02-16 02:11:34 +0100

    footer position absolute

--- scripts/app.js
+++ scripts/app.js
@@ -10,21 +10,26 @@ iconeToggle.addEventListener("click" , toggleMenu);
 //-------------Fonction pour remonter en haut de la page-------------//
 // Fonction pour remonter en haut de la page
 function scrollTopFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Afficher ou masquer le bouton en fonction de la position de défilement
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var btn = document.getElementById("scrollTopBtn");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
   }
-  
-  // Afficher ou masquer le bouton en fonction de la position de défilement
-  window.onscroll = function() {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    var btn = document.getElementById("scrollTopBtn");
-  
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      btn.style.display = "block";
-    } else {
-      btn.style.display = "none";
-    }
-  }
+
+  // Cacher le bouton lorsque la largeur de l'écran est inférieure à 900px
+  // if (window.innerWidth < 400) {
+  //   btn.style.display = "none";
+  // }
+}
